009dce69e88d8e89c8005984164e1cd335f7a7d2
Jasmine Tai <jtai@jtai.ca>
2020-01-19 21:42:07 -0800

    Separate code into binary and library

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,13 @@
 use std::env;
 
-pub use crate::context::*;
-pub use crate::error::*;
-pub use crate::interpreter::*;
-pub use crate::lexer::*;
-pub use crate::parser::*;
-
-mod context;
-mod error;
-mod interpreter;
-mod lexer;
-mod parser;
+use pcalc::{eval, lex, parse, Context};
 
 fn main() {
     let context = Context::default();
     for s in env::args().skip(1) {
         println!("{}", s);
-        let tokens = lexer::lex(&s);
-        let expr = parser::parse(tokens);
+        let tokens = lex(&s);
+        let expr = parse(tokens);
         let val = eval(&expr, &context).unwrap();
         println!("  = {}", context.display(val));
     }
