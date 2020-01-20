eb81e4e005cf069278be3af010b28a36919c48f3
James Tai <jtai@jtai.ca>
2020-01-19 21:42:07 -0800

    Separate code into binary and library

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+pub use crate::context::*;
+pub use crate::error::*;
+pub use crate::interpreter::*;
+pub use crate::lexer::*;
+pub use crate::parser::*;
+
+mod context;
+mod error;
+mod interpreter;
+mod lexer;
+mod parser;
