d2039282367c14c10d0ded83b674c3f94ca54a07
Jasmine Tai <jtai@jtai.ca>
2020-01-19 20:58:21 -0800

    Use scientific notation for small/large numbers
    This adds an option to the Context struct that allows you to choose a
    range outside of which numbers are displayed in scientific notation.

--- src/math/context.rs
+++ src/math/context.rs
@@ -1,6 +1,26 @@
-#[derive(Clone, Debug, Default, PartialEq, Eq)]
+use std::fmt;
+use std::fmt::{Display, Formatter};
+
+#[derive(Clone, Debug, PartialEq)]
 pub struct Context {
     pub angle: AngleMeasure,
+    pub notation_range: (f64, f64),
+}
+
+impl Context {
+    /// Format a number for displaying purposes.
+    pub fn display<'a>(&'a self, num: f64) -> impl Display + 'a {
+        Format { ctx: self, num }
+    }
+}
+
+impl Default for Context {
+    fn default() -> Self {
+        Context {
+            angle: Default::default(),
+            notation_range: (1.0e-3, 1.0e+7),
+        }
+    }
 }
 
 /// Angle measurement unit: degrees or radians.
@@ -29,5 +49,27 @@ impl AngleMeasure {
 impl Default for AngleMeasure {
     fn default() -> Self {
         AngleMeasure::Radians
+    }
+}
+
+// Formatting numbers
+
+struct Format<'a> {
+    ctx: &'a Context,
+    num: f64,
+}
+
+impl Display for Format<'_> {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        if self.ctx.notation_range.0 < self.num && self.num < self.ctx.notation_range.1 {
+            write!(f, "{}", self.num)
+        } else if self.num < 1.0 {
+            write!(f, "{:e}", self.num)
+        } else {
+            // Force '+' on exponent
+            let s = format!("{:e}", self.num);
+            let e = s.find('e').unwrap();
+            write!(f, "{}e+{}", &s[..e], &s[(e + 1)..])
+        }
     }
 }
