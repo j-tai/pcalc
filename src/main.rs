e5d46fee44beb5e2b6170bc1c8de32489a458f2c
Jasmine Tai <jtai@jtai.ca>
2020-01-19 17:28:04 -0800

    Implement simple evaluation of command-line args

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
+use std::env;
+
+use crate::math::Context;
+
 mod lexer;
 mod math;
 mod parser;
 
 fn main() {
-    println!("Hello, world!");
+    for s in env::args().skip(1) {
+        println!("{}", s);
+        let tokens = lexer::lex(&s);
+        let expr = parser::parse(tokens);
+        let context = Context::default();
+        println!("  = {}", expr.eval(&context).unwrap());
+    }
 }
