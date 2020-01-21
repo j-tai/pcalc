7ab050ce0e5d7a74483bc3009193c93285d94454
James Tai <jtai@jtai.ca>
2020-01-20 19:42:28 -0800

    Add REPL when run with no arguments

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,31 @@
 use std::env;
+use std::io;
+use std::io::{BufRead, BufReader};
 
 use pcalc::{eval, lex, parse, Context};
 
 fn main() {
     let context = Context::default();
+
+    let mut did_calc = false;
     for s in env::args().skip(1) {
         println!("{}", s);
         let tokens = lex(&s);
         let expr = parse(tokens);
         let val = eval(&expr, &context).unwrap();
         println!("  = {}", context.display(val));
+        did_calc = true;
+    }
+
+    if !did_calc {
+        // Run REPL
+        let stdin = io::stdin();
+        for line in BufReader::new(stdin).lines() {
+            let line = line.unwrap();
+            let tokens = lex(&line);
+            let expr = parse(tokens);
+            let val = eval(&expr, &context).unwrap();
+            println!("  = {}", context.display(val));
+        }
     }
 }
