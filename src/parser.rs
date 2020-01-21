e8224257b9890432727ad8d0dfddc795345ee8a1
James Tai <jtai@jtai.ca>
2020-01-21 14:09:02 -0800

    Increment all parser levels by one

--- src/parser.rs
+++ src/parser.rs
@@ -7,14 +7,14 @@ use crate::{Expression, Token};
 #[cfg(test)]
 mod tests;
 
-/// Parse a zeroth-level expression: variable assignment.
-fn parse_0<'a>(it: &mut Peekable<impl Iterator<Item = Token<'a>>>) -> Expression {
-    let expr = parse_1(it);
+/// Parse a first-level expression: variable assignment.
+fn parse_1<'a>(it: &mut Peekable<impl Iterator<Item = Token<'a>>>) -> Expression {
+    let expr = parse_2(it);
     if let Some(&Token::Equals) = it.peek() {
         it.next();
         match expr {
             Expression::Var(s) => {
-                let rhs = parse_1(it);
+                let rhs = parse_2(it);
                 Expression::Let(s, Box::new(rhs))
             }
             _ => panic!("Unexpected Equals token"),
@@ -24,15 +24,15 @@ fn parse_0<'a>(it: &mut Peekable<impl Iterator<Item = Token<'a>>>) -> Expression
     }
 }
 
-/// Parse a first-level expression: addition and subtraction.
-fn parse_1<'a>(it: &mut Peekable<impl Iterator<Item = Token<'a>>>) -> Expression {
-    let mut expr = parse_2(it);
+/// Parse a second-level expression: addition and subtraction.
+fn parse_2<'a>(it: &mut Peekable<impl Iterator<Item = Token<'a>>>) -> Expression {
+    let mut expr = parse_3(it);
     // Keep grabbing additions and subtractions (left associative)
     loop {
         match it.peek() {
             Some(Token::Plus) => {
                 it.next();
-                let rhs = parse_2(it);
+                let rhs = parse_3(it);
                 if let Expression::Add(ref mut v) = expr {
                     v.push(rhs);
                 } else {
@@ -41,7 +41,7 @@ fn parse_1<'a>(it: &mut Peekable<impl Iterator<Item = Token<'a>>>) -> Expression
             }
             Some(Token::Minus) => {
                 it.next();
-                let rhs = parse_2(it);
+                let rhs = parse_3(it);
                 expr = Expression::Sub(Box::new([expr, rhs]));
             }
             _ => break,
@@ -50,15 +50,15 @@ fn parse_1<'a>(it: &mut Peekable<impl Iterator<Item = Token<'a>>>) -> Expression
     expr
 }
 
-/// Parse a second-level expression: multiplication and division.
-fn parse_2<'a>(it: &mut Peekable<impl Iterator<Item = Token<'a>>>) -> Expression {
-    let mut expr = parse_3(it);
+/// Parse a third-level expression: multiplication and division.
+fn parse_3<'a>(it: &mut Peekable<impl Iterator<Item = Token<'a>>>) -> Expression {
+    let mut expr = parse_4(it);
     // Keep grabbing multiplications and divisions (left associative)
     loop {
         match it.peek() {
             Some(Token::Times) => {
                 it.next();
-                let rhs = parse_3(it);
+                let rhs = parse_4(it);
                 if let Expression::Mul(ref mut v) = expr {
                     v.push(rhs);
                 } else {
@@ -67,7 +67,7 @@ fn parse_2<'a>(it: &mut Peekable<impl Iterator<Item = Token<'a>>>) -> Expression
             }
             Some(Token::Divide) => {
                 it.next();
-                let rhs = parse_3(it);
+                let rhs = parse_4(it);
                 expr = Expression::Frac(Box::new([expr, rhs]));
             }
             _ => break,
@@ -76,46 +76,46 @@ fn parse_2<'a>(it: &mut Peekable<impl Iterator<Item = Token<'a>>>) -> Expression
     expr
 }
 
-/// Parse a third-level expression: exponentiation.
-fn parse_3<'a>(it: &mut Peekable<impl Iterator<Item = Token<'a>>>) -> Expression {
-    let lhs = parse_4(it);
+/// Parse a fourth-level expression: exponentiation.
+fn parse_4<'a>(it: &mut Peekable<impl Iterator<Item = Token<'a>>>) -> Expression {
+    let lhs = parse_5(it);
     match it.peek() {
         Some(Token::Exponent) => {
             it.next();
             // Right associative
-            let rhs = parse_3(it);
+            let rhs = parse_4(it);
             Expression::Exp(Box::new([lhs, rhs]))
         }
         _ => lhs,
     }
 }
 
-/// Parse a fourth-level expression: functions and prefix unary operators.
-fn parse_4<'a>(it: &mut Peekable<impl Iterator<Item = Token<'a>>>) -> Expression {
+/// Parse a fifth-level expression: functions and prefix unary operators.
+fn parse_5<'a>(it: &mut Peekable<impl Iterator<Item = Token<'a>>>) -> Expression {
     match it.peek() {
         Some(Token::Minus) => {
             it.next();
-            Expression::Neg(Box::new(parse_4(it)))
+            Expression::Neg(Box::new(parse_5(it)))
         }
         Some(Token::Ident(id)) => {
             if let Ok(func) = id.parse() {
                 it.next();
-                let expr = parse_5(it);
+                let expr = parse_6(it);
                 Expression::Func(func, Box::new(expr))
             } else {
-                parse_5(it)
+                parse_6(it)
             }
         }
-        _ => parse_5(it),
+        _ => parse_6(it),
     }
 }
 
-/// Parse a fifth-level expression: numeric literals and parentheses.
-fn parse_5<'a>(it: &mut Peekable<impl Iterator<Item = Token<'a>>>) -> Expression {
+/// Parse a sixth-level expression: numeric literals and parentheses.
+fn parse_6<'a>(it: &mut Peekable<impl Iterator<Item = Token<'a>>>) -> Expression {
     let tok = it.next().expect("unexpected end of input");
     match tok {
         Token::LeftParen => {
-            let expr = parse_1(it);
+            let expr = parse_2(it);
             assert_eq!(it.next(), Some(Token::RightParen));
             expr
         }
@@ -131,12 +131,7 @@ fn parse_5<'a>(it: &mut Peekable<impl Iterator<Item = Token<'a>>>) -> Expression
     }
 }
 
-// /// Parse a sixth-level expression: a fallback for an invalid expression.
-// fn parse_6<'a>(it: &mut impl Iterator<Item = Token<'a>>) -> Expression {
-//     todo!()
-// }
-
 /// Parse an expression into an abstract syntax tree.
 pub fn parse<'a>(it: impl Iterator<Item = Token<'a>>) -> Expression {
-    parse_0(&mut it.fuse().peekable())
+    parse_1(&mut it.fuse().peekable())
 }
