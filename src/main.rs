7ef78dfc81e670e61481b7e4231c22088cd0a82a
James Tai <jtai@jtai.ca>
2020-01-21 21:59:01 -0800

    Ignore empty expressions in command-line and REPL

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,23 @@ use std::io::{BufRead, BufReader};
 
 use pcalc::{eval, lex, parse, Context};
 
+fn run_expr(expr: &str, ctx: &mut Context) {
+    if expr.is_empty() {
+        return;
+    }
+    let tokens = lex(&expr);
+    let expr = parse(tokens);
+    let val = eval(&expr, ctx).unwrap();
+    println!("  = {}", ctx.display(val));
+}
+
 fn main() {
     let mut context = Context::default();
 
     let mut did_calc = false;
     for s in env::args().skip(1) {
         println!("{}", s);
-        let tokens = lex(&s);
-        let expr = parse(tokens);
-        let val = eval(&expr, &mut context).unwrap();
-        println!("  = {}", context.display(val));
+        run_expr(&s, &mut context);
         did_calc = true;
     }
 
@@ -22,10 +29,7 @@ fn main() {
         let stdin = io::stdin();
         for line in BufReader::new(stdin).lines() {
             let line = line.unwrap();
-            let tokens = lex(&line);
-            let expr = parse(tokens);
-            let val = eval(&expr, &mut context).unwrap();
-            println!("  = {}", context.display(val));
+            run_expr(&line, &mut context);
         }
     }
 }
