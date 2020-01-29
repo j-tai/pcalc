60a87e589179172334a599b06a268325243b162e
Jasmine Tai <jtai@jtai.ca>
2020-01-28 22:25:24 -0800

    Use CLI args to run files before opening REPL
    This replaces the old functionality, where command-line arguments
    denoted expressions to be evaluated, with the ability to run files from
    the command-line before opening the REPL.

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,68 @@
 use std::env;
+use std::fs;
 use std::io;
 use std::io::{BufRead, BufReader};
+use std::process::exit;
 
-use pcalc::{eval, lex, parse, Context, Error, Span};
+use pcalc::{eval, lex, parse, Context, Error, Result, Span, Value};
 
-fn show_err(err: Error, span: Span) {
-    for _ in 1..span.start {
-        eprint!(" ");
+fn show_err(err: Error, span: Span, arrow: bool) {
+    if arrow {
+        for _ in 1..span.start {
+            eprint!(" ");
+        }
+        for _ in span.start..=span.end {
+            eprint!("^");
+        }
+        eprintln!();
     }
-    for _ in span.start..=span.end {
-        eprint!("^");
-    }
-    eprintln!();
     eprintln!("{}: {}", span, err);
 }
 
-fn run_expr(expr: &str, ctx: &mut Context, line: u32) {
-    if expr.is_empty() {
-        return;
-    }
-    let tokens = lex(&expr, None);
-    let expr = match parse(tokens) {
-        Ok(e) => e,
-        Err((err, span)) => {
-            show_err(err, span);
-            return;
-        }
-    };
-    let val = match eval(&expr, ctx) {
-        Ok(e) => e,
-        Err((err, span)) => {
-            show_err(err, span);
-            return;
-        }
-    };
-    ctx.vars.insert(format!("ans{}", line), val.clone());
-    println!("  ans{:<4} = {}", line, ctx.display(&val));
-    if let Some(v) = ctx.vars.get_mut("ans") {
-        *v = val;
-    } else {
-        ctx.vars.insert("ans".to_string(), val);
-    }
+fn run_expr(expr: &str, ctx: &mut Context, filename: Option<String>) -> Result<Value> {
+    let tokens = lex(&expr, filename);
+    let expr = parse(tokens)?;
+    eval(&expr, ctx)
 }
 
 fn main() {
-    let mut context = Context::default();
+    let mut ctx = Context::default();
 
-    let mut did_calc = false;
-    let mut line_num = 1;
-    for s in env::args().skip(1) {
-        println!("{}", s);
-        run_expr(&s, &mut context, line_num);
-        did_calc = true;
-        line_num += 1;
+    // Read each file from command-line arguments
+    for filename in env::args().skip(1) {
+        eprintln!("Loading {}...", filename);
+        let s = match fs::read_to_string(&filename) {
+            Ok(s) => s,
+            Err(e) => {
+                eprintln!("pcalc: cannot read {:?}: {}", filename, e);
+                exit(1);
+            }
+        };
+        match run_expr(&s, &mut ctx, Some(filename)) {
+            Ok(_) => (),
+            Err((e, s)) => show_err(e, s, false),
+        }
     }
+    eprintln!("Welcome to pcalc.");
 
-    if !did_calc {
-        // Run REPL
-        let stdin = io::stdin();
-        for line in BufReader::new(stdin).lines() {
-            let line = line.unwrap();
-            run_expr(&line, &mut context, line_num);
-            line_num += 1;
+    // Run REPL
+    let stdin = io::stdin();
+    let mut line_num = 1_u32;
+    for line in BufReader::new(stdin).lines() {
+        let line = line.unwrap();
+        match run_expr(&line, &mut ctx, None) {
+            Ok(v) => {
+                let ans_name = format!("ans{}", line_num);
+                println!("  ans{:<4} = {}", line_num, ctx.display(&v));
+                ctx.vars.insert(ans_name, v.clone());
+                if let Some(ans) = ctx.vars.get_mut("ans") {
+                    *ans = v;
+                } else {
+                    ctx.vars.insert("ans".to_string(), v);
+                }
+                line_num += 1;
+            }
+            Err((e, s)) => show_err(e, s, true),
         }
     }
 }
